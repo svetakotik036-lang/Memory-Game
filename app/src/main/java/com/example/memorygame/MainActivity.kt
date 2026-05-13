e924d6b79704eb7d1ecbba3c9d8bcbdd491d14c9
Rusu Polina <polina.nasibulina@gmail.com>
2026-05-13 15:51:11 +0300

    UI: Implementare identitate vizuală și resurse grafice

--- app/src/main/java/com/example/memorygame/MainActivity.kt
+++ app/src/main/java/com/example/memorygame/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.memorygame.ui.screens.MemoryApp
 
-// ================= UI COMPONENTS =================
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
